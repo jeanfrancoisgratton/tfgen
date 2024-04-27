24d372ae2cd8dae5d2e5363cb095758600589a7b
jeanfrancoisgratton <jean-francois@famillegratton.net>
2024-04-27 02:50:11 -0400

    Changed the output from binary to package

--- __debian/3.restore_repo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git restore control preinst prerm postinst postrm
-rm -rf "tfgen"*
---
+++ debug/main.go
@@ -0,0 +1,32 @@
+// tfgen
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original filename: debug/main.go
+// Original timestamp: 2024/04/27 02:48
+
+// ===========================================
+// WARNING  WARNING  WARNING  WARNING  WARNING
+// ===========================================
+// THIS IS FOR DEBUGGING PURPOSES ONLY. ONCE READY TO PUBLISH, COMMENT
+// ALL FILES IN THIS DIRECTORY
+// ===========================================
+// WARNING  WARNING  WARNING  WARNING  WARNING
+// ===========================================
+
+//package main
+//
+//import (
+//	"fmt"
+//	"github.com/jeanfrancoisgratton/helperFunctions"
+//)
+//
+//func main() {
+//	var intVar int = -19
+//	var int32Var int32 = -65535
+//	var uint16Var uint16 = 55326
+//	var uint64Var uint64 = 12345678901
+//
+//	fmt.Println(helperFunctions.SI(intVar))
+//	fmt.Println(helperFunctions.SI(int32Var))
+//	fmt.Println(helperFunctions.SI(uint16Var))
+//	fmt.Println(helperFunctions.SI(uint64Var))
+//}
